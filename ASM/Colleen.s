7dabebb6ba7b8ec8d421e806780faa560a531955
Maris GASPA <gmaris@e2r3p10.clusters.42paris.fr>
2022-04-23 21:03:07 +0200

    file cerate work

--- ASM/Colleen.s
+++ ASM/Colleen.s
@@ -1,4 +1,4 @@
-;Colleen.s
+;Colleen.s by gmaris
 extern printf
 global main
 section .text
@@ -13,4 +13,4 @@ call printf WRT ..plt
 pop rbx
 ret
 buff:
-db ";Colleen.s%2$cextern printf%2$cglobal main%2$csection .text%2$cmain:%2$cpush rbx%2$clea rdi, [rel buff] ;arg 0 format%2$clea rsi, [rel buff] ;arg 1 data%2$cmov rdx, 10 ;arg 2 new line%2$cmov rcx, 34 ;arg 3 double quote%2$cxor rax, rax ;avoid printf crash%2$ccall printf WRT ..plt%2$cpop rbx%2$cret%2$cbuff:%2$cdb %3$c%1$s%3$c, 0%2$c", 0
+db ";Colleen.s by gmaris%2$cextern printf%2$cglobal main%2$csection .text%2$cmain:%2$cpush rbx%2$clea rdi, [rel buff] ;arg 0 format%2$clea rsi, [rel buff] ;arg 1 data%2$cmov rdx, 10 ;arg 2 new line%2$cmov rcx, 34 ;arg 3 double quote%2$cxor rax, rax ;avoid printf crash%2$ccall printf WRT ..plt%2$cpop rbx%2$cret%2$cbuff:%2$cdb %3$c%1$s%3$c, 0%2$c", 0
