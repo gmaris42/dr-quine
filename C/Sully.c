003d542c47b906cbdb5f5af3fb474c0f13a14251
Maris GASPA <gmaris@e2r3p10.clusters.42paris.fr>
2022-04-23 13:44:36 +0200

    fix sully for linux

--- C/Sully.c
+++ C/Sully.c
@@ -3,11 +3,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 int main() {
-int i = 5;
+int i = 50;
 if (!access("Sully_5.c", R_OK)) --i;
-char buff[] = "#include <stdio.h>%c#include <string.h>%c#include <stdlib.h>%c#include <unistd.h>%cint main() {%cint i = %d;%cif (!access(%cSully_5.c%c, R_OK)) --i;%cchar buff[] = %c%s%c;%cchar name[64];%cchar ex[64];%csprintf(name, %cSully_%%d.c%c, i);%csprintf(ex, %cSully_%%d%c, i);%cFILE *fd = fopen(name, %cw%c);%cfprintf(fd, buff, 10, 10, 10, 10, 10, i, 10, 34, 34, 10, 34, buff, 34, 10, 10, 10, 34, 34, 10, 34, 34, 10, 34, 34, 10, 10, 10, 10, 10, 10, 34, 34, 10, 10, 10);%cfclose(fd);%cif (i == 0)%creturn 0;%cchar cmd[128];%csprintf(cmd, %cgcc -Wall -Werror -Wextra %%s -o %%s; ./%%s%c, name, ex, ex);%csystem(cmd);%c}%c";
-char name[64];
-char ex[64];
+char buff[] = "#include <stdio.h>%c#include <string.h>%c#include <stdlib.h>%c#include <unistd.h>%cint main() {%cint i = %d;%cif (!access(%cSully_5.c%c, R_OK)) --i;%cchar buff[] = %c%s%c;%cchar name[154];%cchar ex[200];%csprintf(name, %cSully_%%d.c%c, i);%csprintf(ex, %cSully_%%d%c, i);%cFILE *fd = fopen(name, %cw%c);%cfprintf(fd, buff, 10, 10, 10, 10, 10, i, 10, 34, 34, 10, 34, buff, 34, 10, 10, 10, 34, 34, 10, 34, 34, 10, 34, 34, 10, 10, 10, 10, 10, 10, 34, 34, 10, 10, 10);%cfclose(fd);%cif (i == 0)%creturn 0;%cchar cmd[800];%csprintf(cmd, %cgcc -Wall -Werror -Wextra %%s -o %%s; ./%%s%c, name, ex, ex);%csystem(cmd);%c}%c";
+char name[154];
+char ex[200];
 sprintf(name, "Sully_%d.c", i);
 sprintf(ex, "Sully_%d", i);
 FILE *fd = fopen(name, "w");
@@ -15,7 +15,7 @@ fprintf(fd, buff, 10, 10, 10, 10, 10, i, 10, 34, 34, 10, 34, buff, 34, 10, 10, 1
 fclose(fd);
 if (i == 0)
 return 0;
-char cmd[128];
+char cmd[800];
 sprintf(cmd, "gcc -Wall -Werror -Wextra %s -o %s; ./%s", name, ex, ex);
 system(cmd);
 }
