631ada36c2fe15fd25b631e38ee2d91aef27c3ef
Maris GASPA <gmaris@e2r3p10.clusters.42paris.fr>
2022-04-23 17:57:13 +0200

    fix asm colleen

--- ASM/Colleen.s
+++ ASM/Colleen.s
@@ -1,13 +1,14 @@
 ;Outside comment
-extern _printf
+extern printf
+global main
 section .text
-_main:
+main:
 lea rdi, [rel buff] ;arg 0 format
 lea rsi, [rel buff] ;arg 1 data
 mov rdx, 10 ;arg 2 new line
 mov rcx, 34 ;arg 3 double quote
-xor rax, rax
-call _printf
+xor rax, rax ;avoid printf crash
+call printf WRT ..plt
 ret
 buff:
-db ";Outside comment%2$cextern _printf%2$csection .text%2$c_main:%2$clea rdi, [rel buff] ;arg 0 format%2$clea rsi, [rel buff] ;arg 1 data%2$cmov rdx, 10 ;arg 2 new line%2$cmov rcx, 34 ;arg 3 double quote%2$cxor rax, rax%2$ccall _printf%2$cret%2$cbuff:%2$cdb %3$c %1$s %3$c, 0", 0
+db ";Outside comment%2$cextern printf%2$cglobal main%2$csection .text%2$cmain:%2$clea rdi, [rel buff] ;arg 0 format%2$clea rsi, [rel buff] ;arg 1 data%2$cmov rdx, 10 ;arg 2 new line%2$cmov rcx, 34 ;arg 3 double quote%2$cxor rax, rax ;avoid printf crash%2$ccall printf WRT ..plt%2$cret%2$cbuff:%2$cdb %3$c%1$s%3$c, 0%2$c", 0
