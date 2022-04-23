d66aecd9720cd9f5e5ac37c5da2e5e2f97ef1bca
Maris GASPA <gmaris@e2r3p10.clusters.42paris.fr>
2022-04-23 16:42:59 +0200

    add makefile

--- C/Colleen.c
+++ C/Colleen.c
@@ -2,11 +2,11 @@
 /*
 	Outside comment.
 */
-void collenn(void) {}
+void colleen(void) {}
 int main(void) {
-	char buff[] = "#include <stdio.h>%c/*%c%cOutside comment.%c*/%cvoid collenn(void) {}%cint main(void) {%c%cchar buff[] = %c%s%c;%c%cprintf(buff, 10, 10, 9, 10, 10, 10, 10, 9, 34, buff, 34, 10, 9, 10, 9, 10, 10, 9, 10, 10, 10);%c%ccollenn();%c/*%c%cInside comment.%c*/%c}%c";
+	char buff[] = "#include <stdio.h>%c/*%c%cOutside comment.%c*/%cvoid colleen(void) {}%cint main(void) {%c%cchar buff[] = %c%s%c;%c%cprintf(buff, 10, 10, 9, 10, 10, 10, 10, 9, 34, buff, 34, 10, 9, 10, 9, 10, 10, 9, 10, 10, 10);%c%ccolleen();%c/*%c%cInside comment.%c*/%c}%c";
 	printf(buff, 10, 10, 9, 10, 10, 10, 10, 9, 34, buff, 34, 10, 9, 10, 9, 10, 10, 9, 10, 10, 10);
-	collenn();
+	colleen();
 /*
 	Inside comment.
 */
